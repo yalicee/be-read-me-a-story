061e04cb25c1580c2fca80c7cc781511b15b9888
akflds <andrewkirsfelds@gmail.com>
2022-06-29 14:08:19 +0100

    bring db connection into app and create example endpoint

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 from flask import Flask
+from firebase_admin import db
+from db.connection import app
 
 app = Flask(__name__)
-
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
-
 @app.route('/json', methods=['POST', 'GET'])
 def test_json():
     return '{"code": 1, "message": "Hello, World!" }'
+
+@app.route('/stories', methods=['GET'])
+def get_stories():
+    stories = db.reference("/stories")
+    return stories.get()
