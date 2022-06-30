8eb9b1d8c36e45d41ac7f06fec6b82aa6bf2882e
Rob <Talon@Emmas-MacBook-Pro.local>
2022-06-30 16:11:54 +0100

    Testing for getting the stories complete

--- __tests__/test_app.py
+++ __tests__/test_app.py
@@ -17,11 +17,21 @@ def test_app_error():
 def test_get_stories():
     url = "http://localhost:9000/.json?ns=stories"
     res = requests.get(url)
-    # resDict = json.loads(res)
-    # keyslist = list(resDict.keys())
+    story = res.json()["26437e71-8993-4025-a049-686d875fd0e1"]
+    storyDict = res.json()
+    storyKeys = storyDict.keys()
+    storyList = list(storyKeys)
+
     assert res.status_code == 200
-    assert res.json()["26437e71-8993-4025-a049-686d875fd0e1"]['title']== "The Twelve Princesses"
-    # assert type(json.loads(res)) is dict 
-    # assert res.json()[]
-# Loop over keyslist and assert the res.json
-# check the shape of each story object within the json
+    assert story['title']== "The Twelve Princesses"
+    assert story['created_at']== 1656575857
+    assert story['created_by']== "andy@email.com"
+    assert story["cover"]== "gs:/path/to/firebase/image.jpg"
+    assert story["families"]== {"fid_0": True,"fid_1": True}
+    assert story["chapters"]== [{
+        "chapter_src": "gs:/path/to/recording/file.ogg",
+        "created_by": "andy@email.com",
+        "played": False
+    }]
+    assert len(storyList) == 2
+    assert type (storyDict) is dict
