29f0c7cd54755ef4cda1e1a6ae68ccf9052e3d3a
Rob <Talon@Emmas-MacBook-Pro.local>
2022-06-30 11:35:34 +0100

    added test data file

--- __tests__/test_app.py
+++ __tests__/test_app.py
@@ -1,11 +1,10 @@
 import requests
-
+import json
 
 def test_app():
     url = "http://127.0.0.1:5000"
     res = requests.get(url)
     assert res.status_code == 200
-    print(res.text)
     assert res.text == "<p>Hello, World!</p>"
 
 
@@ -14,3 +13,15 @@ def test_app_error():
     res = requests.get(url)
     assert res.status_code == 404
 
+
+def test_get_stories():
+    url = "http://127.0.0.1:5000/stories"
+    res = requests.get(url)
+    # resDict = json.loads(res)
+    # keyslist = list(resDict.keys())
+    assert res.status_code == 200
+    assert res.json()["26437e71-8993-4025-a049-686d875fd0e1"]['title']== "The Twelve Princesses"
+    # assert type(json.loads(res)) is dict 
+    # assert res.json()[]
+# Loop over keyslist and assert the res.json
+# check the shape of each story object within the json
---
+++ test-data/stories.py
@@ -0,0 +1,36 @@
+{
+  "26437e71-8993-4025-a049-686d875fd0e1": {
+    "chapters": [
+      {
+        "chapter_src": "gs:/path/to/recording/file.ogg",
+        "created_by": "andy@email.com",
+        "played": False
+      }
+    ],
+    "cover": "gs:/path/to/firebase/image.jpg",
+    "created_at": 1656575857,
+    "created_by": "andy@email.com",
+    "families": {
+      "fid_0": True,
+      "fid_1": True
+    },
+    "title": "The Twelve Princesses"
+  },
+  "327201ce-1ae6-4c38-8400-203c65ec6927": {
+    "chapters": [
+      {
+        "chapter_src": "gs:/path/to/recording/file.ogg",
+        "created_by": "andy@email.com",
+        "played": False
+      }
+    ],
+    "cover": "gs:/path/to/firebase/image.jpg",
+    "created_at": 1656578924,
+    "created_by": "andy@email.com",
+    "families": {
+      "fid_0": True,
+      "fid_1": True
+    },
+    "title": "The Twelve Princesses"
+  }
+}
