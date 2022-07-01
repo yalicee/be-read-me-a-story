8ff97658b4cffaf9bc5db3c916522b5270f50aac
Alex Yang <alex.yang1115@gmail>
2022-07-01 15:14:37 +0100

    implement post a story

--- app.py
+++ app.py
@@ -18,28 +18,26 @@ def hello_world():
 
 @app.route('/stories', methods=['POST'])
 @cross_origin()
-def stories():
+def create_story():
+    story = json.loads(request.data)
     if request.method == "POST":
-        newStory = db.reference("stories/" + str(uuid.uuid4()))
-        res = newStory.set({
-            "title": "The Twelve Princesses",
-            "created_by": "andy@email.com",
+        new_story = db.reference("stories/" + str(uuid.uuid4()))
+        res = new_story.set({
+            "title": story["title"],
+            "created_by": story["userId"],
             "created_at": int(time.time()),
             "cover": "gs:/path/to/firebase/image.jpg",
             "chapters": [
                 {
-                    "created_by": "andy@email.com",
-                    "chapter_src": "gs:/path/to/recording/file.ogg",
+                    "created_by": story["userId"],
+                    "chapter_src": story["chapterSource"],
                     "played": False
                 }
             ],
             "families": {
-                "fid_0": True,
-                "fid_1": True
+                story["familyId"]: True
             }
         })
         response = app.response_class(
-            response=json.dumps(newStory.get()), status=201, mimetype="application/json")
-        print(response)
-        print(newStory.get())
+            response=json.dumps(new_story.get()), status=201, mimetype="application/json")
         return response
