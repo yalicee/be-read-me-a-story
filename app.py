c2f87f7c17904fc7cc6ee72043436f00d2f45043
akflds <andrewkirsfelds@gmail.com>
2022-07-04 10:40:55 +0100

    refactor create_story to throw error

--- app.py
+++ app.py
@@ -19,26 +19,27 @@ def create_story():
     story = json.loads(request.data)
     if request.method == "POST":
         new_story = db.reference("stories/" + str(uuid.uuid4()))
-        res = new_story.set({
-            "title": story["title"],
-            "created_by": story["userId"],
-            "created_at": int(time.time()),
-            "cover": "gs:/path/to/firebase/image.jpg",
-            "chapters": [
-                {
-                    "created_by": story["userId"],
-                    "chapter_src": story["chapterSource"],
-                    "played": False
+        try: 
+            new_story.set({
+                "title": story["title"],
+                "created_by": story["userId"],
+                "created_at": int(time.time()),
+                "cover": "gs:/path/to/firebase/image.jpg",
+                "chapters": [
+                    {
+                        "created_by": story["userId"],
+                        "chapter_src": story["chapterSource"],
+                        "played": False
+                    }
+                ],
+                "families": {
+                    story["familyId"]: True
                 }
-            ],
-            "families": {
-                story["familyId"]: True
-            }
-        })
-        response = app.response_class(
-            response=json.dumps(new_story.get()), status=201, mimetype="application/json")
-        return response
-
+            })
+            return jsonify(new_story.get()), 201
+        except: 
+            return jsonify({"msg": "Could not create story"}), 400
+ 
 @app.route('/stories/<family_id>', methods=['GET'])
 @cross_origin()
 def get_stories_by_family(family_id):
