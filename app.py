e59a0bdd3a8b4997e6468d2740a568b1442fb4f4
akflds <andrewkirsfelds@gmail.com>
2022-07-08 09:08:22 +0100

    proper cross origin

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask, request, jsonify
 from firebase_admin import db
-from flask_cors import cross_origin
+from flask_cors import CORS, cross_origin
 from db.connection import app
 import uuid
 import time
 import json
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 
 @app.route("/")
